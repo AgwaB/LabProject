6dbf039b9a20e3ab15f1fd67941921b20315a4bb
KSJ <leesd556@gmail.com>
2017-12-30 17:17:27 +0900

    first commit

---
+++ app/src/main/java/com/example/user/labproject/SelectSituationActivity.java
@@ -0,0 +1,42 @@
+package com.example.user.labproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by user on 2017-12-29.
+ */
+
+public class SelectSituationActivity extends Activity {
+    ImageView sink, Void;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_situation);
+
+        sink = (ImageView)findViewById(R.id.to_activity_sink);
+        Void = (ImageView)findViewById(R.id.to_activity_void);
+
+
+        sink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SinkEnterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Void.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),VoidEnterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
